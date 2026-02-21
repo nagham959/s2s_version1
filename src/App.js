3d8247f35c2f2d2334865bae94fbda0b02b537af
Tahamohamed11 <taham3830@gmail.com>
2026-02-21 14:21:13 +0200

    Fix login password issue and update username/email on new account registration

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import SignUpPage from "./pages/SignUpPage";
 import DashboardPage from "./pages/DashboardPage";
 import ProfileSettingsPage from "./pages/ProfileSettingsPage";
 import VerifyEmailPage from "./pages/verifyEmailPage";
-import HistoryPage from './pages/HistoryPage';
+import HistoryPage from "./pages/HistoryPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ChangePasswordPage from './pages/ChangePasswordPage';
+import ChangePasswordPage from "./pages/ChangePasswordPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from './contexts/ThemeContext';
-import { HistoryProvider } from './contexts/HistoryContext';
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { HistoryProvider } from "./contexts/HistoryContext";
 
 const queryClient = new QueryClient();
 
@@ -22,30 +22,32 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-      <HistoryProvider> 
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
+        <HistoryProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<HomePage />} />
 
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/verifyEmail" element={<VerifyEmailPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/verifyEmail" element={<VerifyEmailPage />} />
 
-          <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+              <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
+              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
-          <Route path="/resetPassword" element={<ResetPasswordPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
+              <Route path="/resetPassword" element={<ResetPasswordPage />} />
+              <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/profile-settings" element={<ProfileSettingsPage />} />
-          <Route path="/change-password" element={<ChangePasswordPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-
-        </Routes>
-      </Router>
-      </HistoryProvider>
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route
+                path="/profile-settings"
+                element={<ProfileSettingsPage />}
+              />
+              <Route path="/change-password" element={<ChangePasswordPage />} />
+              <Route path="/history" element={<HistoryPage />} />
+            </Routes>
+          </Router>
+        </HistoryProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
